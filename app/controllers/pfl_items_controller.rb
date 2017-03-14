7fd832402c179129463420f51a33b53b228e4dee
joshuajazleung <joshuajazleung@gmail.com>
2017-03-14 16:48:49 -0700

    integreted new and create actions for portfolios

--- app/controllers/pfl_items_controller.rb
+++ app/controllers/pfl_items_controller.rb
@@ -2,4 +2,20 @@ class PflItemsController < ApplicationController
   def index
     @portfolio_items = PflItem.all
   end
+
+  def new
+    @portfolio_item = PflItem.new
+  end
+
+  def create
+    @portfolio_item = PflItem.new(params.require(:pfl_item).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to pfl_items_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
