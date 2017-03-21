59d0c20ebb03c3f9f6745f374da48dde384d456a
joshuajazleung <joshuajazleung@gmail.com>
2017-03-21 16:08:35 -0700

    implememnted concern for managing image data

--- app/models/pfl_item.rb
+++ app/models/pfl_item.rb
@@ -1,3 +1,11 @@
 class PflItem < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: "600", width: "400");
+    self.thumb_image ||= Placeholder.image_generator(height: "350", width: "250");
+  end
 end
