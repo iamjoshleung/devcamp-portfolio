e47601dbc6c5a0e09ed2b03156ed2f83758942bf
joshuajazleung <joshuajazleung@gmail.com>
2017-04-03 16:17:04 -0700

    implemented partial for admin action

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -3,6 +3,12 @@ module BlogsHelper
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
   end
 
+  def blog_status_color blog 
+    if blog.draft?
+      'color: red;'
+    end
+  end
+
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
       CodeRay.scan(code, language).div
